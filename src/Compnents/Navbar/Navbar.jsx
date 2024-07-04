682377ad0344a727a213565291e87c030945efa5
tusharsharma2822 <tusharsharma2822@gmail.com>
2024-07-04 13:23:48 +0530

    Portfolio created using React + vite

--- src/Compnents/Navbar/Navbar.jsx
+++ src/Compnents/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
@@ -9,14 +9,24 @@ import menu_close from '../../assets/menu_close.svg';
 
 const Navbar = () => {
 
-  const [menu,setMenu] = useState("Home")
+  const [menu,setMenu] = useState("Home");
+
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right="0";
+  }
+
+  const closeMenu = () => {
+    menuRef.current.style.right="-350px";
+  }
 
   return (
     <div className='navbar'>
         <img src={logo} alt='logo' className='logo'/>
-        <img src={menu_open} alt=''/>
-        <ul className='nav-menu'>
-          <img src={menu_close} alt='' className='nav-mob-close' />
+        <img src={menu_open} onClick={openMenu} alt='' className='nav-mob-open'/>
+        <ul ref={menuRef} className='nav-menu'>
+          <img src={menu_close} onClick={closeMenu} alt='' className='nav-mob-close' />
           <li className='home'> <AnchorLink className='anchor-link' href='#Home'><p onClick={()=>setMenu("Home")}>Home</p></AnchorLink>{menu==="Home"?<img src={underline} alt=''/>: <></>}</li>
           <li className='about'><AnchorLink className='anchor-link' offset={50} href='#About'><p onClick={()=>setMenu("About")}>About Me</p></AnchorLink>{menu==="About"?<img src={underline} alt=''/>: <></>}</li>
           <li className='portfolio'><AnchorLink className='anchor-link' offset={50} href='#Portfolio'><p onClick={()=>setMenu("Portfolio")}>Portfolio</p></AnchorLink>{menu==="Portfolio"?<img src={underline} alt=''/>: <></>}</li>
